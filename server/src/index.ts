d1aa56bcf8ebda9958a34d6e80dafcc2bdb7959e
val-suprunovska <val-suprunovska@ukr.net>
2025-09-18 00:15:03 +0300

    fix: ports

--- server/src/index.ts
+++ server/src/index.ts
@@ -7,7 +7,7 @@ import routes from './routes';
 dotenv.config();
 
 const app = express();
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 10000;
 
 // Безопасное получение URL фронтенда
 const nodeEnv = process.env.NODE_ENV || 'development';
